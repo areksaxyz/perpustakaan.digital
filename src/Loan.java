77533b3c1ca7b7ebf2c53a6dc1da0dbaec19c16c
NOB CODER <m.argareksapati21@gmail.com>
2025-04-16 21:05:41 +0700

    Update Loan.java

--- src/Loan.java
+++ src/Loan.java
@@ -4,19 +4,27 @@ public class Loan {
     private String loanId;
     private String bookIsbn;
     private String borrowerName;
+    private String borrowerClass;
+    private String nim;
     private LocalDate borrowDate;
     private LocalDate dueDate;
-    private boolean isReturned;
+    private LocalDate returnDate;
+    private boolean returned;
     private double fine;
+    private boolean finePaid;
 
-    public Loan(String loanId, String bookIsbn, String borrowerName, LocalDate borrowDate, LocalDate dueDate) {
+    public Loan(String loanId, String bookIsbn, String borrowerName, String borrowerClass, String nim, LocalDate borrowDate, LocalDate dueDate) {
         this.loanId = loanId;
         this.bookIsbn = bookIsbn;
         this.borrowerName = borrowerName;
+        this.borrowerClass = borrowerClass;
+        this.nim = nim;
         this.borrowDate = borrowDate;
         this.dueDate = dueDate;
-        this.isReturned = false;
+        this.returned = false;
+        this.returnDate = null;
         this.fine = 0.0;
+        this.finePaid = false;
     }
 
     public String getLoanId() {
@@ -31,6 +39,22 @@ public class Loan {
         return borrowerName;
     }
 
+    public String getBorrowerClass() {
+        return borrowerClass;
+    }
+
+    public void setBorrowerClass(String borrowerClass) {
+        this.borrowerClass = borrowerClass;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
     public LocalDate getBorrowDate() {
         return borrowDate;
     }
@@ -39,23 +63,39 @@ public class Loan {
         return dueDate;
     }
 
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
     public boolean isReturned() {
-        return isReturned;
+        return returned;
+    }
+
+    public void setReturned(boolean returned) {
+        this.returned = returned;
     }
 
     public double getFine() {
         return fine;
     }
 
-    public void setDueDate(LocalDate dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public void setReturned(boolean returned) {
-        isReturned = returned;
-    }
-
     public void setFine(double fine) {
         this.fine = fine;
+    }
+
+    public boolean isFinePaid() {
+        return finePaid;
+    }
+
+    public void setFinePaid(boolean finePaid) {
+        this.finePaid = finePaid;
+    }
+
+    public void extendDueDate(int days) {
+        this.dueDate = this.dueDate.plusDays(days);
     }
 }
